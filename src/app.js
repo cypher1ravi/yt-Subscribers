ff1fa9a1d54e818cecb82025997eb4940e9d76c6
ravindra <123213831+cypher1ravi@users.noreply.github.com>
2024-02-26 19:14:13 +0530

    initial commit

---
+++ src/app.js
@@ -0,0 +1,39 @@
+
+const express = require('express');
+const Subscribers = require('./models/subscribers');
+const documentation = require('./swagger');
+const app = express()
+
+app.get('/subscriber', async (req, res) => {
+    try {
+        const subscribers = await Subscribers.find()
+        res.json(subscribers)
+    } catch (error) {
+        res.send(500).json({ message: error.message })
+    }
+});
+app.get('/subscriber/name', async (req, res) => {
+    try {
+        const subscribers = await Subscribers.find({}, { name: 1, subscribedChannel: 1, _id: 0 })
+        res.json(subscribers)
+    } catch (error) {
+        res.send(500).json({ message: error.message })
+    }
+});
+app.get('/subscriber/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const subscriber = await Subscribers.findById(id);
+        if (!subscriber) {
+            return res.status(400).json({ message: 'Subscriber not found' });
+        }
+        res.json(subscriber);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+
+});
+app.use('/', documentation);
+
+
+module.exports = app;
